06d764b53cc26fc22fe6e71db5bd2e99516e5bfd
Luan DeMello <melloluan8@gmail.com>
2020-11-05 11:32:12 -0500

    initial commit

---
+++ src/script.js
@@ -0,0 +1,17 @@
+//Import Jquery Module
+import $ from 'jquery';
+
+// Import Styles
+import 'normalize.css';
+import './style.css';
+
+//Import API & Store Modules 
+import api from './api';
+import store from './store';
+import bookLi from './bookmark-list';
+
+function main() {
+
+}
+
+$(main);
